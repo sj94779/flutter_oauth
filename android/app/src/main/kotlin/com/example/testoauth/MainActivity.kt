8fa436388566a3d5ebae7a5ba998cb5eae5971e2
sj_dev <shalini.jain@blattnertech.com>
2024-09-02 17:31:19 +0530

    First commit

---
+++ android/app/src/main/kotlin/com/example/testoauth/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.testoauth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,174 @@
+/// -----------------------------------
+///          External Packages
+/// -----------------------------------
+
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+import 'package:auth0_flutter/auth0_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+const appScheme = 'flutterdemo';
+
+/// -----------------------------------
+///           Profile Widget
+/// -----------------------------------
+
+class Profile extends StatelessWidget {
+  final Future<void> Function() logoutAction;
+  final UserProfile? user;
+
+  const Profile(this.logoutAction, this.user, {final Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 150,
+          height: 150,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.blue, width: 4),
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(user?.pictureUrl.toString() ?? ''),
+            ),
+          ),
+        ),
+        const SizedBox(height: 24),
+        Text('Name: ${user?.name}'),
+        const SizedBox(height: 48),
+        ElevatedButton(
+          onPressed: () async {
+            await logoutAction();
+          },
+          child: const Text('Logout'),
+        ),
+      ],
+    );
+  }
+}
+
+/// -----------------------------------
+///            Login Widget
+/// -----------------------------------
+
+class Login extends StatelessWidget {
+  final Future<void> Function() loginAction;
+  final String loginError;
+
+  const Login(this.loginAction, this.loginError, {final Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: () async {
+            await loginAction();
+          },
+          child: const Text('Login'),
+        ),
+        Text(loginError ?? ''),
+      ],
+    );
+  }
+}
+
+/// -----------------------------------
+///                 App
+/// -----------------------------------
+
+Future<void>  main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({final Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+/// -----------------------------------
+///              App State
+/// -----------------------------------
+
+class _MyAppState extends State<MyApp> {
+  Credentials? _credentials;
+  late Auth0 auth0;
+
+  bool isBusy = false;
+  late String errorMessage;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Auth0 Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Auth0 Demo'),
+        ),
+        body: Center(
+          child: isBusy
+              ? const CircularProgressIndicator()
+              : _credentials != null
+              ? Profile(logoutAction, _credentials?.user)
+              : Login(loginAction, errorMessage),
+        ),
+      ),
+    );
+  }
+
+  Future<void> loginAction() async {
+    setState(() {
+      isBusy = true;
+      errorMessage = '';
+    });
+
+    try {
+      var credentials = await auth0
+          .webAuthentication(scheme: appScheme)
+      // Use a Universal Link callback URL on iOS 17.4+ / macOS 14.4+
+      // useHTTPS is ignored on Android
+          .login();
+
+      log(credentials.accessToken);
+//
+//       final Credentials credentials = await auth0.webAuthentication(scheme: appScheme).login(useHTTPS: true);
+// print("credentials:${credentials.accessToken}");
+      setState(() {
+        isBusy = false;
+        _credentials = credentials;
+      });
+    } on Exception catch (e, s) {
+      debugPrint('login error: $e - stack: $s');
+
+      setState(() {
+        isBusy = false;
+        errorMessage = e.toString();
+      });
+    }
+  }
+
+  Future<void> logoutAction() async {
+    var response = await auth0.webAuthentication(scheme: appScheme).logout();
+
+    setState(() {
+      _credentials = null;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    auth0 = Auth0(dotenv.env['DOMAIN']!, dotenv.env['CLIENT_ID']!);
+    errorMessage = '';
+  }
+}
